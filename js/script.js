c894db6ee5f7c9ff44b34982a97b20d62ea2fe2e
Victor,K.M <kuchikivic@gmail.com>
2018-06-22 16:17:24 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// setTimeout(function(){
+//   var elem = document.querySelector('#nav');
+//   elem.className = 'fixed';
+// }, 1000)
